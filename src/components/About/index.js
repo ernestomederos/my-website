cc8f91f4a73b116772bb179c4fa85aeadb0f4761
Ernesto Mederos <ernesto.mederosgarcia@dci.education>
2023-08-20 16:02:24 +0200

    responsiveness commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,11 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import {
-//     faHtml5,
-//     faJsSquare,
-//     faCss3,
-//     faReact,
-//     faGitAlt,
-//     faAngular
-// } from '@fortawesome/free-brands-svg-icons';
-import Loader from 'react-loaders'
+import { faLinkedin, faGithub, faSkype } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Loader from 'react-loaders';
 
-const About = () => {
+export default function About() {
     const [letterClass, setLetterClass] = useState('text-animate');
     const myFunctionSetTimeOut = async () => {
         return setTimeout(() => {
@@ -35,14 +28,41 @@ const About = () => {
                             idx={15} />
                     </h1>
                     <p>
-                        Fueled by an unquenchable thirst for knowledge and innovation, I embrace challenges as stepping stones to growth.
+                        « I started my programming journey when I was fifteen, delving into the intricacies of coding logic even before owning a computer. Back then, I tackled school coding assignments on paper, a practice that I still enjoy for its nostalgic charm.»
+
                     </p>
                     <p>
-                        With each project, I strive to push boundaries and craft solutions that transcend expectations.
+                        « Growing up in the always-unstable environment of Havana instilled in me the adaptability of a water bear, enabling me to thrive amid challenges. Now, I find myself in Berlin, driven by a fervent desire to contribute as a Web Developer. I aspire to weave solutions, forge connections, implement innovations, and envision technologies that propel humanity forward. »
                     </p>
-                    <p>
-                        My journey as a web developer is marked by relentless determination, and I find purpose in creating digital experiences that leave a lasting impact on users and the industry alike.
-                    </p>
+                    <ul>
+                        <li>
+                            <a
+                                target="_blank"
+                                rel='noreferrer'
+                                href='https://www.linkedin.com/in/ernestomederosdev/'
+                            >
+                                <FontAwesomeIcon icon={faLinkedin} color="#fff" />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                target="_blank"
+                                rel='noreferrer'
+                                href='https://github.com/ernestomederos'
+                            >
+                                <FontAwesomeIcon icon={faGithub} color="#fff" />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                target="_blank"
+                                rel='noreferrer'
+                                href='skype:live:.cid.68502857057a1a31'
+                            >
+                                <FontAwesomeIcon icon={faSkype} color="#fff" />
+                            </a>
+                        </li>
+                    </ul>
                 </div>
 
 
@@ -51,5 +71,3 @@ const About = () => {
         </>
     )
 }
-
-export default About;
